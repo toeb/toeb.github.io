4b7a34bab5041961d5e3a06f691ca4fd5d82877d
Tobias Becker <becker.tobi@gmail.com>
2019-01-24 21:25:29 +0100

    ...

--- app/polymer/polymer.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('toebgithubio')
-  .controller('PolymerCtrl', ['$scope','appService', function ($scope, appService) {
-    $scope.date = new Date();
-    $scope.app = appService;
-    
-  }]);
